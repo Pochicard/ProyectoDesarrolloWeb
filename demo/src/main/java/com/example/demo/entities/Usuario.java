f921fb643c85d436b6ba0a7ebaf5ef6368d1fc82
santiago0217 <santiagojaviersilvacaliman@gmail.com>
2026-09-16 22:13:23 -0500

    La entidad Usuario ahora usa Lombok

--- demo/src/main/java/com/example/demo/entities/Usuario.java
+++ demo/src/main/java/com/example/demo/entities/Usuario.java
@@ -1,7 +1,13 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "usuario")
 public class Usuario {
@@ -21,55 +27,4 @@ public class Usuario {
 
     @Column(nullable = false)
     private Boolean activo = true;
-
-    public Usuario() {
-    }
-
-    public Usuario(Long id, String nombre, String correo, String telefono, Boolean activo) {
-        this.id = id;
-        this.nombre = nombre;
-        this.correo = correo;
-        this.telefono = telefono;
-        this.activo = activo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public Boolean getActivo() {
-        return activo;
-    }
-
-    public void setActivo(Boolean activo) {
-        this.activo = activo;
-    }
 }
